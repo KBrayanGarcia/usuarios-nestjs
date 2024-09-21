ce55ecda0854caf45d3f928393b43cd8265b0b49
Brayan Garcia <brayangapkb@gmail.com>
2024-09-21 17:57:40 -0600

    FEAT: Se realizaron los siguientes ajustes:
    - Se agregaron nuevos campos a la entidad de usuario, todavìa sin generar la migración

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -6,41 +6,50 @@ export class CreateUserDto implements UserInsert {
     @IsString({ message: "El nombre debe ser una cadena de texto" })
     @IsNotEmpty({ message: "El nombre es requerido" })
     @MaxLength(255, { message: "El nombre no puede tener más de 255 caracteres" })
-    @ApiProperty({ description: "Nombre del usuario", example: "Juan Perez", required: true, type: String })
-    name: string;
+    @ApiProperty({ description: "Nombre del usuario", example: "Juan", required: true, type: String })
+    nombre: string;
+
+    @IsString({ message: "El apellido paterno debe ser una cadena de texto" })
+    @IsNotEmpty({ message: "El apellido paterno es requerido" })
+    @MaxLength(255, { message: "El apellido paterno no puede tener más de 255 caracteres" })
+    @ApiProperty({ description: "Apellido paterno del usuario", example: "Pérez", required: true, type: String })
+    apellido_paterno: string;
+
+    @IsString({ message: "El apellido materno debe ser una cadena de texto" })
+    @MaxLength(255, { message: "El apellido materno no puede tener más de 255 caracteres" })
+    @ApiProperty({ description: "Apellido materno del usuario", example: "García", required: false, type: String })
+    apellido_materno?: string;
 
     @IsEmail({}, { message: "El correo electrónico debe ser válido" })
-    @IsNotEmpty({ message: "El correo electrónico es requerido" })
     @MaxLength(255, { message: "El correo electrónico no puede tener más de 255 caracteres" })
     @ApiProperty({
         description: "Correo electrónico del usuario",
         example: "juan@example.com",
-        required: true,
+        required: false,
         type: String,
     })
     email: string;
+
+    @IsString({ message: "El nombre completo debe ser una cadena de texto" })
+    @MaxLength(255, { message: "El nombre completo no puede tener más de 255 caracteres" })
+    @ApiProperty({
+        description: "Nombre completo del usuario",
+        example: "Juan Pérez García",
+        required: false,
+        type: String,
+        readOnly: true,
+    })
+    nombre_completo: string;
+
+    @IsString({ message: "El usuario debe ser una cadena de texto" })
+    @IsNotEmpty({ message: "El usuario es requerido" })
+    @MaxLength(255, { message: "El usuario no puede tener más de 255 caracteres" })
+    @ApiProperty({ description: "Usuario del usuario", example: "juanperez", required: true, type: String })
+    usuario: string;
 
     @IsString({ message: "La contraseña debe ser una cadena de texto" })
     @IsNotEmpty({ message: "La contraseña es requerida" })
     @MaxLength(255, { message: "La contraseña no puede tener más de 255 caracteres" })
     @ApiProperty({ description: "Contraseña del usuario", example: "password123", required: true, type: String })
     password: string;
-
-    @IsString({ message: "El nombre debe ser una cadena de texto" })
-    @IsNotEmpty({ message: "El nombre es requerido" })
-    @MaxLength(255, { message: "El nombre no puede tener más de 255 caracteres" })
-    @ApiProperty({ description: "Nombre del usuario", example: "Juan Perez", required: true, type: String })
-    first_name: string;
-
-    @IsString({ message: "El nombre debe ser una cadena de texto" })
-    @IsNotEmpty({ message: "El nombre es requerido" })
-    @MaxLength(255, { message: "El nombre no puede tener más de 255 caracteres" })
-    @ApiProperty({ description: "Nombre del usuario", example: "Juan Perez", required: true, type: String })
-    last_name: string;
-
-    @IsString({ message: "El nombre debe ser una cadena de texto" })
-    @IsNotEmpty({ message: "El nombre es requerido" })
-    @MaxLength(255, { message: "El nombre no puede tener más de 255 caracteres" })
-    @ApiProperty({ description: "Nombre del usuario", example: "Juan Perez", required: true, type: String })
-    fullname: string;
 }
--- src/modules/users/entity/user.entity.ts
+++ src/modules/users/entity/user.entity.ts
@@ -3,13 +3,15 @@ import { int, mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
 
 export const users_model = mysqlTable("users", {
     id: int("id").primaryKey().autoincrement().notNull(),
-    first_name: varchar("first_name", { length: 255 }).notNull(),
-    last_name: varchar("last_name", { length: 255 }),
-    fullname: varchar("fullname", { length: 255 }).notNull(),
-    email: varchar("email", { length: 255 }).unique().notNull(),
+    nombre: varchar("nombre", { length: 255 }).notNull(),
+    apellido_paterno: varchar("apellido_paterno", { length: 255 }).notNull(),
+    apellido_materno: varchar("apellido_materno", { length: 255 }),
+    nombre_completo: varchar("nombre_completo", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).unique(),
+    usuario: varchar("usuario", { length: 255 }).unique().notNull(),
     password: varchar("password", { length: 255 }).notNull(),
-    createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
-    updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`),
+    fecha_creacion: timestamp("fecha_creacion").default(sql`CURRENT_TIMESTAMP`),
+    fecha_actualizacion: timestamp("fecha_actualizacion").default(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`),
 });
 
 export type User = typeof users_model.$inferSelect;
